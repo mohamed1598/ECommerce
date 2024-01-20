d8b44727902d43db231f0b59d87df663258cd528
mohamed elshafey <elshafeym170@gmail.com>
2024-01-20 14:07:00 +0200

    configuring identiy db and seeding identity defaults

--- shopAPI/Program.cs
+++ shopAPI/Program.cs
@@ -40,7 +40,7 @@ builder.Services.AddCors( opt =>
 {
     opt.AddPolicy("CorsPolicy", policy =>
     {
-        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "http://localhost:63166", "https://localhost:4200");
+        policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200", "http://localhost:63166", "https://localhost:4200","http://localhost:55167");
     });
 }
     
--- shopAPI/obj/Debug/net7.0/shopAPI.AssemblyInfo.cs
+++ shopAPI/obj/Debug/net7.0/shopAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("shopAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+559659265a106011bb0b0b9a252f5cda17b8633e")]
 [assembly: System.Reflection.AssemblyProductAttribute("shopAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("shopAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
